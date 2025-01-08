c0cd3374902a220bf73132a8e65fe46b35861d3c
andrepsilvadev <pintodasilva.a@gmail.com>
2025-01-08 12:29:01 +0000

    added notes

---
+++ src/notes.R
@@ -0,0 +1,10 @@
+# latest updates
+- SRIT database (trait data collection and habitat suitability is now done separately from the SRIT pipeline). Allows to run SRIT faster. Pipeline has been tested. 
+- selected species are those for which activity area overlaps with IUCN range (not the bufffer as previously done) - too strict?
+- Buffer of activity area is now species-specific (based on home-range)
+
+# suitability module
+#20250108
+- Incorporate global and local drivers of suitability - for instance climatic suitability is not yet incorporated (global driver) 
+run sdms for suitability landscapes
+- a mechanistic estimation of the niche see stefan example with metarange
