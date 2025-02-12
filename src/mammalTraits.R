4155303d70e76f2628d80b0619d0c12942a83da2
afonsobarrocal <77905553+afonsobarrocal@users.noreply.github.com>
2025-02-12 17:36:39 +0000

    Update mammalTraits.R

--- src/mammalTraits.R
+++ src/mammalTraits.R
@@ -1,3 +1,4 @@
+
 ## Mammal traits ##
 ## Afonso Barrocal & Inês Silva ##
 ## February 12th, 2025 ##
@@ -13,7 +14,10 @@
 if (length(list.files(pattern = "mammalTraits_.*\\.csv$")) != 0) {
   mammalTraits_data <- read_csv(list.files(pattern = "mammalTraits_.*\\.csv$"))
 } else {
-
+  
+  ### Printing message
+  cat(bold(red("No file detected:"),green("creating mammal trait database...")))
+  
   ### EXTRACT HOME RANGE DATA ###
   
   # import home range data
@@ -62,7 +66,7 @@ if (length(list.files(pattern = "mammalTraits_.*\\.csv$")) != 0) {
   
   ################################################################################
   
-  ## Step 4 - Extract trait data and build a R dataframe with extracted data
+  ## Extract trait data and build a R dataframe with extracted data
   
   # import traits from the IUCN shapefile
   sf_use_s2(FALSE)
@@ -80,7 +84,7 @@ if (length(list.files(pattern = "mammalTraits_.*\\.csv$")) != 0) {
   
   # download PANTHERIA data
   pantheria <- read_tsv("https://esapubs.org/archive/ecol/E090/184/PanTHERIA_1-0_WR05_Aug2008.txt") %>%
-  replace_with_na_all(condition = ~.x == -999)
+    replace_with_na_all(condition = ~.x == -999)
   
   # import SANTINI et al 2022
   santini2022 <- read_excel("trait_datasets/geb13476-sup-0002-tables1.xls") #reports error of RMSE_bvs instead of numeric (it's ok)
@@ -146,57 +150,14 @@ if (length(list.files(pattern = "mammalTraits_.*\\.csv$")) != 0) {
                   'Diet.Meat', 'Diet.Plant', 'Mass.g', 'max_longevity_d', 'weaning_mass_g',
                   'litter_size_n', 'litters_per_year_n', 'Mating System','PredMd','up75',
                   'trophic_level', 'Max_Home_Range_km2', 'Min_Home_Range_km2') %>%
-    distinct()
-  
-  # process family and order data
-  combined_traits_data$family.x <- str_to_title(combined_traits_data$family.x)
-  combined_traits_data$order_ <- str_to_title(combined_traits_data$order_)
-  
-  # prepared for RangeShifter
-  combined_traits_data <- combined_traits_data %>%
-    rename('Species' = 'sci_name',
-           'Biome' = 'BIOME_NAME',
-           'Region' = 'CONTINENT',
-           'Family' = 'family.x',
-           'Order' = 'order_',
-           'IndsHaCell' = 'PredMd',
-           'TargetHaDensity' = 'up75',
-           'MaxAge' ='max_longevity_d',
-           'Harem' = 'Mating System',
-           'Stage0bodyMass' = 'weaning_mass_g',
-           'Stage1Fecundity' = 'litter_size_n',
-           'RepSeasons' = 'litters_per_year_n',
-           'MaxHomeRange' = 'Max_Home_Range_km2', 
-           'MinHomeRange' = 'Min_Home_Range_km2') %>%
-    mutate(BodyMass = Mass.g/1000,  #convert mass in grams to kilograms
-           Trophic = ifelse(Diet.Meat >= 90, "Carnivore",
-                            ifelse(Diet.Plant >= 90, "Herbivore", "Omnivore")),
-           Stage0Mortality = Stage0bodyMass^-0.25, #based on McCarthy 2008 and Savage 2004
-           Stage1Mortality = BodyMass^-0.25,
-           MeanDisp = ifelse(Trophic == "Carnivore", 3.45*BodyMass^0.89,  #based on Schloss 2012
-                             ifelse(Trophic == "Herbivore", 1.45*BodyMass^0.54, NA)), 
-           LongDisp = ifelse(Trophic == "Carnivore", 40.7*BodyMass^0.81,
-                             ifelse(Trophic == "Herbivore", 3.31*BodyMass^0.65, NA)),
-           MaxAge = MaxAge/365,
-           trophic_level=replace(trophic_level, trophic_level==1, "Herbivore"),
-           trophic_level=replace(trophic_level, trophic_level==2, "Omnivore"),
-           trophic_level=replace(trophic_level, trophic_level==3, "Carnivore")) %>%
-    dplyr::select('Species', 'Family', 'Order', 'Trophic', 'trophic_level',
-                  'BodyMass', 'IndsHaCell', 'TargetHaDensity', 'MaxAge', 'Harem',
-                  'Stage0bodyMass', 'Stage0Mortality', 'Stage1Fecundity', 'Stage1Mortality',
-                  'RepSeasons', 'MeanDisp', 'LongDisp', 'MaxHomeRange', 'Biome', 'Region')
-  
-  # excluded incomplete data
-  final_mammal_traits <- combined_traits_data %>% drop_na()
+    distinct() %>% 
+    drop_na()
   
   # csv file with trait data
-  write.csv(final_mammal_traits, paste0("mammalTraits_",Sys.Date(),".csv"), row.names = FALSE)
+  write.csv(combined_traits_data, paste0("mammalTraits_",Sys.Date(),".csv"), row.names = FALSE)
   
-  complete_data_sps <-data.frame(unique(final_mammal_traits$Species))
+  # summary infor
+  complete_data_sps <-data.frame(unique(combined_traits_data$sci_name))
   names(complete_data_sps) <-"Species"
-  
-  # summary info
-  sps_summarised <- final_mammal_traits %>%
-    group_by(Trophic, trophic_level, Family) %>%
-    summarise(Mean_BodyMass = mean(BodyMass), n = n())
+  print(paste("NºSpecies:",nrow(complete_data_sps)))
 }
