8ed47ac2afcb266a7a626a12df901232a1d4b662
afonsobarrocal <77905553+afonsobarrocal@users.noreply.github.com>
2025-02-17 18:45:03 +0000

    Add files via upload

--- src/treeTraits.R
+++ src/treeTraits.R
@@ -2,16 +2,11 @@
 ## Afonso Barrocal ##
 ## February 17th, 2025 ##
 
-##########################################
-## NOT TESTED YET - JUST AN ONLINE COPY ##
-##########################################
-
 ##########################################################################
 ## Things to change: ##
-##   1. Process trait data (from BIEN);
-##   2. See what are the limiting factors in the database;
-##   3. Make more comments;
-##   4. (...)
+##   1. See what are the limiting factors in the database;
+##   2. Make more comments;
+##   3. (...)
 ##########################################################################
 
 if (length(list.files(pattern = "treeTraits_.*\\.csv$")) != 0) {
@@ -38,15 +33,68 @@ if (length(list.files(pattern = "treeTraits_.*\\.csv$")) != 0) {
     st_drop_geometry()
   invisible(gc())
   
-  # import BIEN data (https://doi.org/10.1111/2041-210X.12861)
+  # import BIEN database (https://doi.org/10.1111/2041-210X.12861)
   bien_traits <- do.call(rbind,lapply(sps_traits$sci_name,
                                       FUN = BIEN_trait_species))
+  # collect trait names and units from BIEN database
+  traits_info <- bien_traits %>% 
+    select(trait_name,unit) %>%
+    distinct() %>%
+    drop_na()
+  
+  # export trait names and units
+  if(!(file.exists("treeTraits_info_units.csv"))){
+    write.csv(traits_info, "treeTraits_info_units.csv")
+  }
+  
+  # remove unnecessary objects to free internal memory
+  rm(traits_info)
+  invisible(gc())
+  
+  # process continuous data from BIEN database
+  tree_traits <- bien_traits %>% 
+    select(scrubbed_species_binomial,
+           trait_name,trait_value) %>%
+    group_by(scrubbed_species_binomial,trait_name) %>%
+    summarise(trait_value = mean(as.numeric(trait_value), na.rm = TRUE)) %>%
+    pivot_wider(names_from = trait_name,
+                values_from = trait_value) %>%
+    select(scrubbed_species_binomial,
+           `diameter at breast height (1.3 m)`,
+           `leaf nitrogen content per leaf dry mass`,
+           `leaf phosphorus content per leaf dry mass`,
+           `plant flowering begin`,
+           `plant flowering duration`,
+           `seed mass`,
+           `stem wood density`,
+           `vessel lumen area`,
+           `vessel number`,
+           `whole plant height`)
+  
+  # process categorical data from BIEN database
+  tree_traits2 <- bien_traits %>%
+    pivot_wider(names_from = trait_name,
+                values_from = trait_value) %>%
+    select(scrubbed_species_binomial,
+           `whole plant sexual system`,
+           `whole plant dispersal syndrome`,
+           `flower pollination syndrome`,
+           `whole plant growth form`,
+           `whole plant woodiness`,
+           `whole plant vegetative phenology`,
+           `whole plant growth form diversity`) %>%
+    unique() 
+  tree_traits2$`whole plant growth form` <- tolower(tree_traits2$`whole plant growth form`)
+  tree_traits2 <- tree_traits2 %>%
+    group_by(scrubbed_species_binomial) %>%
+    summarise(across(everything(), ~ .[order(is.na(.))][1]), .groups = 'drop')
   
   # table with info on the n of species per dataset
   datasets_info <- tibble(
     dataset = c("iucn","iucn+bien"),
     n_sps = c(length(unique(sps_traits$sci_name)),
-              length(unique(bien_traits$scrubbed_species_binomial))))
+              length(unique(tree_traits$scrubbed_species_binomial))+
+              length(unique(tree_traits2$scrubbed_species_binomial))))
   invisible(gc())
   
   # import the biome data
@@ -86,7 +134,8 @@ if (length(list.files(pattern = "treeTraits_.*\\.csv$")) != 0) {
   
   # combining traits
   combined_traits_data <- sps_traits %>%
-    left_join(bien_traits, by = c('sci_name' = 'scrubbed_species_binomial')) %>%
+    left_join(tree_traits, by = c('sci_name' = 'scrubbed_species_binomial')) %>%
+    left_join(tree_traits2, by = c('sci_name' = 'scrubbed_species_binomial')) %>%
     left_join(sps_biome, by = c('sci_name' = 'sci_name')) %>%
     distinct() %>% 
     drop_na()
