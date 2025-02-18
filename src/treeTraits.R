acfee0890bb54dc3186565a7c410da256f661296
afonsobarrocal <77905553+afonsobarrocal@users.noreply.github.com>
2025-02-18 09:01:43 +0000

    Add files via upload

--- src/treeTraits.R
+++ src/treeTraits.R
@@ -1,6 +1,6 @@
 ## Tree traits ##
 ## Afonso Barrocal ##
-## February 17th, 2025 ##
+## February 18th, 2025 ##
 
 ##########################################################################
 ## Things to change: ##
@@ -36,6 +36,7 @@ if (length(list.files(pattern = "treeTraits_.*\\.csv$")) != 0) {
   # import BIEN database (https://doi.org/10.1111/2041-210X.12861)
   bien_traits <- do.call(rbind,lapply(sps_traits$sci_name,
                                       FUN = BIEN_trait_species))
+  
   # collect trait names and units from BIEN database
   traits_info <- bien_traits %>% 
     select(trait_name,unit) %>%
