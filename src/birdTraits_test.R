3383b69a56feabfefa9c9d08025071db70406e52
afonsobarrocal <77905553+afonsobarrocal@users.noreply.github.com>
2025-03-03 09:51:53 +0000

    Add files via upload

--- src/birdTraits_test.R
+++ src/birdTraits_test.R
@@ -1,11 +1,11 @@
 ## Birds traits ##
 ## Afonso Barrocal ##
-## February 19th, 2025 ##
+## March 1st, 2025 ##
 
 ####################################################################################
 ##                                                                                ##
 ## Error:   got 'std::bad_alloc'                                                  ##
-## But it works - the 'ensure_multipolygons works!!!                              ##
+## But it works - the 'ensure_multipolygons' works!!!                             ##
 ## Bad_alloc issue might be solvable by dividing the shapefile into n-folds...    ##
 ## I'll try it by using the 'dismo::kfold' function with an 'if' condition!!!     ##
 ##                                                                                ##
@@ -39,9 +39,30 @@ if (length(list.files(pattern = "birdTraits_.*\\.csv$")) != 0) {
     ensure_multipolygons() %>%
     st_make_valid()
   invisible(gc())
-    
+
+  # create vector with target bird families
+  target_birds_families <- c("Bucerotidae", # hornbills
+                             "Bucorvidae", # hornbills
+                             "Ramphastidae", # toucans
+                             "Picidae") # woodpeckers
+
+  # retrieve species
+  target_bird_species <- taxize::downstream(target_birds_families,
+                                            downto = "species",
+                                            db = "gbif") %>%
+    do.call(rbind,.) %>%
+    dplyr::select(name)
+  target_bird_species <- unique(target_bird_species$name)
+
+  # select target species from IUCN shapefile
+  IUCN_birds_target <- IUCN_birds[IUCN_birds$sci_name %in% target_bird_species,]
+
+  # remove unnecessary objects
+  rm(target_birds_families,target_bird_species)
+  invisible(gc())
+  
   # import traits from the IUCN shapefile
-  sps_traits <- unique(IUCN_birds$sci_name)
+  sps_traits <- unique(IUCN_birds_target$sci_name)
   
   # import AVONET data (https://doi.org/10.1111/ele.13898)
   avonet <- read_excel("trait_datasets/AVONET Supplementary dataset 1.xlsx", 
@@ -66,27 +87,26 @@ if (length(list.files(pattern = "birdTraits_.*\\.csv$")) != 0) {
   invisible(gc())
   
   # import the continent data (https://figshare.com/articles/dataset/Continent_Polygons/12555170)
-  continent <- read_sf("continent/Continents.shp")[,2] %>%
-    st_make_valid()
+  continent <- read_sf("continent/Continents.shp")[,2]
   
   # intersect ecoregions and continents shapefiles
   biomes <- st_intersection(st_make_valid(biomes),st_make_valid(continent))
   invisible(gc())
   
   # intersect IUCN and ecoregions and continents shapefiles
-  biomes <- st_intersection(st_make_valid(biomes),st_make_valid(IUCN_birds))
+  biomes <- st_intersection(st_make_valid(biomes),st_make_valid(IUCN_birds_target))
   invisible(gc())
   
   # subset of the necessary variables (biome and sps scientific names)
   sps_biome <- biomes %>%
     dplyr::select('BIOME_NAME', 'sci_name', "CONTINENT") %>% 
-    dplyr::filter(sci_name %in% total_target_sps) %>%
+  # dplyr::filter(sci_name %in% total_target_sps) %>%
     st_drop_geometry() %>%
     distinct()
   invisible(gc())
   
   # remove unnecessary objects to increase internal memory
-  rm(biomes,continent,IUCN_birds)
+  rm(biomes,continent,IUCN_birds_target)
   invisible(gc())
   
   ################################################################################
