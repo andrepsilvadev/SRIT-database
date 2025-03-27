28319f8ecc5e2528d984e20a4b9a0e3c814bc501
maria-ines-silva <ncisines@gmail.com>
2025-03-27 09:56:43 +0000

    update traits notes (trees, birds and mammals)

--- src/notes.R
+++ src/notes.R
@@ -1,3 +1,12 @@
+# trait data updates
+#20250327 (MIS)
+- Tree traits are still imcomplete see next links for more info on databases (used and to still look at)
+    - Script: https://github.com/andrepsilvadev/SRIT-database/blob/4e124479c5dd238a17ad78b4e3c4f069a4e388a9/src/TreeTraits_MIS.R
+    - Databases files: https://drive.google.com/drive/folders/1VppQMKqJOlJigZMws-3BWcO2tUUmQ9KA?usp=sharing
+    - Tree traits to still look for: https://docs.google.com/spreadsheets/d/1JHLHvyIlQKSPwfPvWB8TYRV6UA62H5cb5gUQIZzmPEE/edit?usp=sharing
+- Mammals traits are good (130 sps with complete trait data for tropical and boreal forests together)
+- Bird traits could still benefit from a search for max ages (or longevity) and ages at maturity/reproduction ages, so we can get more than woodpeckers and the tucan
+    
 # latest updates
 - SRIT database (trait data collection and habitat suitability is now done separately from the SRIT pipeline). Allows to run SRIT faster. Pipeline has been tested. 
 - selected species are those for which activity area overlaps with IUCN range (not the bufffer as previously done) - too strict?
