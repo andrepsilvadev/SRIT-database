f1f314a7ea951c055a0e4465a0215a8ed393ae41
maria-ines-silva <misilva@ciencias.ulisboa.pt>
2025-07-25 12:22:37 +0100

    re-do birdTraits Database script with new databases

--- src/birdTraits.R
+++ src/birdTraits.R
@@ -1,111 +1,94 @@
 ## Birds traits ##
-## Afonso Barrocal ##
-## February 19th, 2025 ##
-
-####################################################################################
-##                                                                                ##
-## Error:   got 'std::bad_alloc'                                                  ##
-## But it works - the 'ensure_multipolygons works!!!                              ##
-## Bad_alloc issue might be solvable by dividing the shapefile into n-folds...    ##
-## I'll try it by using the 'dismo::kfold' function with an 'if' condition!!!     ##
-##                                                                                ##
-####################################################################################
-
-##########################################################################
-## Things to change: ##
-##   1. Update code so that it doesn't have to make spatial processing;
-##   2. Check Wang et al. (2017) database;
-##   3. Search for tropical trait dataset;
-##   4. See what are the limiting factors in the database;
-##   5. Make more comments;
-##   6. (...)
-##########################################################################
+## Inês Silva & Afonso Barrocal ##
+## 19 July 2025 ##
 
 if (length(list.files(pattern = "birdTraits_.*\\.csv$")) != 0) {
   birdTraits_data <- read_csv(list.files(pattern = "birdTraits_.*\\.csv$"))
 } else {
   
-  ### Printing message
+  # Printing message
   cat(bold(red("No file detected:"),green("creating bird trait database...")))
   
-  ## Extract trait data and build a R dataframe with extracted data
+  # get started from an RDS object saved in this repo
+  birds_biomes <- read.csv("./trait_datasets/birdTraits/birds_intersection_biomes_2025-07-25.csv") %>% 
+    rename(sci_name = sci_nam)
   
-  # work on a flat Earth
-  sf_use_s2(FALSE)
-  
-  # import bird distribution data from BirdLife (IUCN)
-  IUCN_birds <- read_sf("trait_datasets/species/BOTW_2024_2.gpkg",
-                        promote_to_multi = T, type = 0) %>%
-    ensure_multipolygons() %>%
-    st_make_valid()
-  invisible(gc())
-    
-  # import traits from the IUCN shapefile
-  sps_traits <- unique(IUCN_birds$sci_name)
+  # alternatively use the .rds object
+  # birds_biomes <- readRDS(file = "birds_biomes.rds") %>% 
+  #   rename(sci_name = sci_nam) %>% 
+  #   st_drop_geometry()
+  # invisible(gc())
   
   # import AVONET data (https://doi.org/10.1111/ele.13898)
-  avonet <- read_excel("trait_datasets/AVONET Supplementary dataset 1.xlsx", 
-                       sheet = "AVONET1_BirdLife", na = "NA")
-  
-  # import Storchova & Hořák traits (https://doi.org/10.1111/geb.12709)
-  storchova <- read_excel("trait_datasets/Storchova_traitsdata.xlsx", 
-                                     sheet = "Sheet2", na = "NA")
-  
-  # table with info on the n of species per dataset
-  datasets_info <- tibble(
-    dataset = c("avonet", "storchova"),
-    n_sps = c(length(unique(avonet$Species1)), length(unique(storchova$Species))))
+  avonet <- read_excel("./trait_datasets/birdTraits/externalData/AVONET Supplementary dataset 1.xlsx", 
+                       sheet = "AVONET1_BirdLife", na = "NA") %>% 
+    # select important columns 
+    dplyr::select(Species1, Trophic.Level) %>% # Range.Size comes from this database
+    dplyr::mutate(
+      Trophic_level_mod = ifelse(Trophic.Level == "Scavenger", "Carnivore", Trophic.Level))
   invisible(gc())
   
-  # import the biome data
-  biomes <- read_sf("ecoregions/Ecoregions2017.shp") %>%
-    replace_with_na(list(BIOME_NAME = c("N/A"))) %>%
-    select('BIOME_NAME') %>%
-    distinct() %>%
-    drop_na()
+  # import santini et al. 2023 (https://onlinelibrary.wiley.com/doi/10.1111/geb.13688)
+  santini2023 <- read_csv("./trait_datasets/birdTraits/externalData/geb13688-sup-0003-appendixs2.csv") %>% 
+    # select important columns 
+    dplyr::select(Species, `Predicted_Density_n_km2`, Q75) %>% 
+    mutate(Species = str_replace(Species, "_", " "))
   invisible(gc())
   
-  # import the continent data (https://figshare.com/articles/dataset/Continent_Polygons/12555170)
-  continent <- read_sf("continent/Continents.shp")[,2] %>%
-    st_make_valid()
-  
-  # intersect ecoregions and continents shapefiles
-  biomes <- st_intersection(st_make_valid(biomes),st_make_valid(continent))
+  # import Jetz et al 2008 (https://journals.plos.org/plosbiology/article?id=10.1371/journal.pbio.0060303)
+  jetz2008 <- read_excel("./trait_datasets/birdTraits/externalData/jetz2008_avianClutch.xlsx") %>% 
+    # select important columns (remaining ones are just references which are not all in the same column)
+    dplyr::select(Order, Family, Genus, Species, Clutch) %>% 
+    mutate(sci_name = paste0(Genus," ", Species))
   invisible(gc())
   
-  # intersect IUCN and ecoregions and continents shapefiles
-  biomes <- st_intersection(st_make_valid(biomes),st_make_valid(IUCN_birds))
+  # import AMNIOTE database (https://esajournals.onlinelibrary.wiley.com/doi/10.1890/15-0846R.1)
+  Amniote2015 <- read_csv("./trait_datasets/birdTraits/externalData/Amniote_Database_Aug_2015.csv") %>%
+    naniar::replace_with_na_all(condition = ~.x == -999) %>%
+    mutate(sci_name = paste0(genus," ", species))
   invisible(gc())
   
-  # subset of the necessary variables (biome and sps scientific names)
-  sps_biome <- biomes %>%
-    dplyr::select('BIOME_NAME', 'sci_name', "CONTINENT") %>% 
-    dplyr::filter(sci_name %in% total_target_sps) %>%
-    st_drop_geometry() %>%
-    distinct()
+  # import Bird et al 2020 (https://conbio.onlinelibrary.wiley.com/doi/epdf/10.1111/cobi.13486)
+  bird_et_al_2020 <- read_excel("./trait_datasets/birdTraits/externalData/cobi13486-sup-0002-tables2.xlsx") %>% 
+    # select important columns 
+    dplyr::select(Species, Adult_survival_M, Age_at_first_reproduction_M, Maximum_longevity_M)
   invisible(gc())
   
-  # remove unnecessary objects to increase internal memory
-  rm(biomes,continent,IUCN_birds)
-  invisible(gc())
-  
-  ################################################################################
-  ## FINAL DATASET ##
-  
-  # combining traits
-  combined_traits_data <- sps_traits %>%
-    left_join(storchova, by = c('sci_name' = 'Species')) %>%
-    left_join(avonet, by = c("sci_name" = "Species1")) %>%
-    left_join(sps_biome, by = c('sci_name' = 'sci_name')) %>%
+  ## FINAL DATABASE ##
+  combined_birdTraits <- birds_biomes %>% 
+    left_join(avonet, by = c("sci_name" = "Species1")) %>% # for trophic level
+    left_join(santini2023, by = c("sci_name" = "Species")) %>% # for abundance and carrying capacity
+    left_join(jetz2008, by = c("sci_name" = "sci_name")) %>%  # for clutch 
+    left_join(bird_et_al_2020, by = c("sci_name" = "Species")) %>% # for max age, age of first breeding and survival rate
+    left_join(Amniote2015, by = c("sci_name" = "sci_name")) %>%  # for adult body mass (grams)
+    dplyr::select(sci_name, BIOME_NAME, CONTINENT,
+                  Order, Family,
+                  # clutch size
+                  Clutch,
+                  # mass (g) & trophic level
+                  adult_body_mass_g, adult_body_mass_g, Trophic.Level, Trophic_level_mod,
+                  # habitat info
+                  BIOME_NAME, CONTINENT,
+                  # abundance & carrying capacity (no. indiv per km2)
+                  Predicted_Density_n_km2, Q75,
+                  # age & survival rates
+                  Adult_survival_M, Age_at_first_reproduction_M, Maximum_longevity_M) %>% 
     distinct() %>% 
-    drop_na()
+    drop_na(c(Clutch, adult_body_mass_g, Predicted_Density_n_km2,
+              Q75, Adult_survival_M, Age_at_first_reproduction_M,
+              Maximum_longevity_M ))
   
   # csv file with trait data
-  write.csv(combined_traits_data, paste0("birdTraits_",Sys.Date(),".csv"), row.names = FALSE)
+  write.csv(combined_birdTraits, paste0("birdTraits_",Sys.Date(),".csv"), row.names = FALSE)
   
-  # summary infor
-  complete_data_sps <-data.frame(unique(combined_traits_data$sci_name))
-  names(complete_data_sps) <-"Species"
-  print(paste("NºSpecies:",nrow(complete_data_sps)))
+  # final check-ups & messages
+  cat("No. of species:", length(unique(combined_birdTraits$sci_name)), "\n") # how many species
   
+  sapply(combined_birdTraits, function(x) sum(is.na(x))) # number NA per column
+  
+  birdsPerBiomeContinet <- combined_birdTraits %>%
+    group_by(BIOME_NAME, CONTINENT) %>%
+    summarise(n_species = n_distinct(sci_name), .groups = "drop") %>%
+    arrange(desc(n_species))
+  View(birdsPerBiomeContinet)
 }
