eb4e5740161a1a2af3763c7f20f033938c175f61
maria-ines-silva <misilva@ciencias.ulisboa.pt>
2025-08-01 11:52:00 +0100

    update birdTraits script

--- src/birdTraits.R
+++ src/birdTraits.R
@@ -2,6 +2,7 @@
 ## Inês Silva & Afonso Barrocal ##
 ## 19 July 2025 ##
 
+setwd("C:/Users/User/OneDrive - Universidade de Lisboa (1)/ANDRE/NatPoKe")
 if (length(list.files(pattern = "birdTraits_.*\\.csv$")) != 0) {
   birdTraits_data <- read_csv(list.files(pattern = "birdTraits_.*\\.csv$"))
 } else {
@@ -10,7 +11,7 @@ if (length(list.files(pattern = "birdTraits_.*\\.csv$")) != 0) {
   cat(bold(red("No file detected:"),green("creating bird trait database...")))
   
   # get started from an RDS object saved in this repo
-  birds_biomes <- read.csv("./trait_datasets/birdTraits/birds_intersection_biomes_2025-07-25.csv") %>% 
+  birds_biomes <- read.csv("./traits/birdTraits/externalData/birds_intersection_biomes_2025-07-25.csv") %>% 
     rename(sci_name = sci_nam)
   
   # alternatively use the .rds object
@@ -20,7 +21,7 @@ if (length(list.files(pattern = "birdTraits_.*\\.csv$")) != 0) {
   # invisible(gc())
   
   # import AVONET data (https://doi.org/10.1111/ele.13898)
-  avonet <- read_excel("./trait_datasets/birdTraits/externalData/AVONET Supplementary dataset 1.xlsx", 
+  avonet <- read_excel("./traits/birdTraits/externalData/AVONET Supplementary dataset 1.xlsx", 
                        sheet = "AVONET1_BirdLife", na = "NA") %>% 
     # select important columns 
     dplyr::select(Species1, Trophic.Level) %>% # Range.Size comes from this database
@@ -29,27 +30,27 @@ if (length(list.files(pattern = "birdTraits_.*\\.csv$")) != 0) {
   invisible(gc())
   
   # import santini et al. 2023 (https://onlinelibrary.wiley.com/doi/10.1111/geb.13688)
-  santini2023 <- read_csv("./trait_datasets/birdTraits/externalData/geb13688-sup-0003-appendixs2.csv") %>% 
+  santini2023 <- read_csv("./traits/birdTraits/externalData/geb13688-sup-0003-appendixs2.csv") %>% 
     # select important columns 
     dplyr::select(Species, `Predicted_Density_n_km2`, Q75) %>% 
     mutate(Species = str_replace(Species, "_", " "))
   invisible(gc())
   
   # import Jetz et al 2008 (https://journals.plos.org/plosbiology/article?id=10.1371/journal.pbio.0060303)
-  jetz2008 <- read_excel("./trait_datasets/birdTraits/externalData/jetz2008_avianClutch.xlsx") %>% 
+  jetz2008 <- read_excel("./traits/birdTraits/externalData/jetz2008_avianClutch.xlsx") %>% 
     # select important columns (remaining ones are just references which are not all in the same column)
     dplyr::select(Order, Family, Genus, Species, Clutch) %>% 
     mutate(sci_name = paste0(Genus," ", Species))
   invisible(gc())
   
   # import AMNIOTE database (https://esajournals.onlinelibrary.wiley.com/doi/10.1890/15-0846R.1)
-  Amniote2015 <- read_csv("./trait_datasets/birdTraits/externalData/Amniote_Database_Aug_2015.csv") %>%
+  Amniote2015 <- read_csv("./traits/birdTraits/externalData/Amniote_Database_Aug_2015.csv") %>%
     naniar::replace_with_na_all(condition = ~.x == -999) %>%
     mutate(sci_name = paste0(genus," ", species))
   invisible(gc())
   
   # import Bird et al 2020 (https://conbio.onlinelibrary.wiley.com/doi/epdf/10.1111/cobi.13486)
-  bird_et_al_2020 <- read_excel("./trait_datasets/birdTraits/externalData/cobi13486-sup-0002-tables2.xlsx") %>% 
+  bird_et_al_2020 <- read_excel("./traits/birdTraits/externalData/cobi13486-sup-0002-tables2.xlsx") %>% 
     # select important columns 
     dplyr::select(Species, Adult_survival_M, Age_at_first_reproduction_M, Maximum_longevity_M)
   invisible(gc())
@@ -74,9 +75,9 @@ if (length(list.files(pattern = "birdTraits_.*\\.csv$")) != 0) {
                   # age & survival rates
                   Adult_survival_M, Age_at_first_reproduction_M, Maximum_longevity_M) %>% 
     distinct() %>% 
-    drop_na(c(Clutch, adult_body_mass_g, Predicted_Density_n_km2,
-              Q75, Adult_survival_M, Age_at_first_reproduction_M,
-              Maximum_longevity_M ))
+    drop_na(c(Clutch, adult_body_mass_g, Trophic_level_mod,
+              Predicted_Density_n_km2, Q75, Adult_survival_M,
+              Age_at_first_reproduction_M, Maximum_longevity_M ))
   
   # csv file with trait data
   write.csv(combined_birdTraits, paste0("birdTraits_",Sys.Date(),".csv"), row.names = FALSE)
